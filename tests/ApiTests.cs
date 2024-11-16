2566cd9362b55fd8c2752697c635f1c38f087d4a
Yousif FJ <47510107+Yousif-FJ@users.noreply.github.com>
2024-11-16 22:38:02 +0200

    Add test framework

---
+++ tests/ApiTests.cs
@@ -0,0 +1,25 @@
+namespace tests;
+
+using System.Text;
+
+[TestClass]
+public class ApiTests
+{
+    public TestContext TestContext {get; set; } = default!;
+
+    [TestMethod]
+    public async Task MakeRequestToRestApi_ShouldReturnOkay()
+    {
+        var httpClient = new HttpClient(){
+            BaseAddress = new Uri("http://localhost:8198/")
+        };
+        var base64EncodedAuthString = Convert.ToBase64String(Encoding.UTF8.GetBytes("admin:admin"));
+        httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + base64EncodedAuthString);
+        
+        var response = await httpClient.GetAsync("/");
+
+        TestContext.WriteLine($"Response: {response.StatusCode}");
+
+        Assert.IsTrue(response.IsSuccessStatusCode);
+    }
+}
