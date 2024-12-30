040fcfcef5fc217681d4c4afddb304e32270b8b0
Yousif FJ <47510107+Yousif-FJ@users.noreply.github.com>
2024-12-31 00:48:25 +0300

    Add tests for login endpoint

--- tests/Tests.cs
+++ tests/Tests.cs
@@ -1,23 +1,25 @@
 namespace tests;
 
+using System.Net;
 using System.Text;
 
 [TestClass]
 public class Tests
 {
-    public TestContext TestContext {get; set; } = default!;
+    public TestContext TestContext { get; set; } = default!;
 
     [TestMethod]
     public async Task MakeGetRequestWithAuthentication_ToRootPage_ShouldReturnOkay()
     {
         var serviceHost = Environment.GetEnvironmentVariable("CICD_APP_SERVICE_HOSTNAME") ?? "localhost";
 
-        var httpClient = new HttpClient(){
+        var httpClient = new HttpClient()
+        {
             BaseAddress = new Uri($"http://{serviceHost}:8198/")
         };
         var base64EncodedAuthString = Convert.ToBase64String(Encoding.UTF8.GetBytes("admin:admin"));
         httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + base64EncodedAuthString);
-        
+
         var response = await httpClient.GetAsync("/");
 
         TestContext.WriteLine($"Response: {response.StatusCode}");
@@ -26,10 +28,49 @@ public class Tests
     }
 
     [TestMethod]
-    public async Task MakeGetRequestTo_ApiRequest_ShouldReturnOkay(){
+    public async Task MakePostLoginRequestWithAuthentication_ToMainPagePort_ShouldReturnOkay()
+    {
         var serviceHost = Environment.GetEnvironmentVariable("CICD_APP_SERVICE_HOSTNAME") ?? "localhost";
 
-        var httpClient = new HttpClient(){
+        var httpClient = new HttpClient()
+        {
+            BaseAddress = new Uri($"http://{serviceHost}:8198/")
+        };
+
+        var base64EncodedAuthString = Convert.ToBase64String(Encoding.UTF8.GetBytes("admin:admin"));
+        httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + base64EncodedAuthString);
+
+        var response = await httpClient.PostAsync("/login", null);
+
+        TestContext.WriteLine($"Response: {response.StatusCode}");
+
+        Assert.IsTrue(response.IsSuccessStatusCode);
+    }
+
+    [TestMethod]
+    public async Task MakePostLoginRequest_ToApiPort_ShouldReturnForbidden()
+    {
+        var serviceHost = Environment.GetEnvironmentVariable("CICD_APP_SERVICE_HOSTNAME") ?? "localhost";
+
+        var httpClient = new HttpClient()
+        {
+            BaseAddress = new Uri($"http://{serviceHost}:8197/")
+        };
+
+        var response = await httpClient.PostAsync("/login", null);
+
+        TestContext.WriteLine($"Response: {response.StatusCode}");
+
+        Assert.IsTrue(response.StatusCode == HttpStatusCode.Forbidden);
+    }
+
+    [TestMethod]
+    public async Task MakeGetRequest_ToApi_ShouldReturnOkay()
+    {
+        var serviceHost = Environment.GetEnvironmentVariable("CICD_APP_SERVICE_HOSTNAME") ?? "localhost";
+
+        var httpClient = new HttpClient()
+        {
             BaseAddress = new Uri($"http://{serviceHost}:8197/")
         };
 
