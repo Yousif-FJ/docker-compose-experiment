aa723e5bc198c2c3cc9ee7bf19333e1e303f3224
Yousif FJ <47510107+Yousif-FJ@users.noreply.github.com>
2024-12-30 21:02:17 +0300

    Add test for API gateway /request

--- tests/Tests.cs
+++ tests/Tests.cs
@@ -3,12 +3,12 @@ namespace tests;
 using System.Text;
 
 [TestClass]
-public class ApiTests
+public class Tests
 {
     public TestContext TestContext {get; set; } = default!;
 
     [TestMethod]
-    public async Task MakeRequestToRestApi_ShouldReturnOkay()
+    public async Task MakeGetRequestWithAuthentication_ToRootPage_ShouldReturnOkay()
     {
         var serviceHost = Environment.GetEnvironmentVariable("CICD_APP_SERVICE_HOSTNAME") ?? "localhost";
 
@@ -21,6 +21,19 @@ public class ApiTests
         var response = await httpClient.GetAsync("/");
 
         TestContext.WriteLine($"Response: {response.StatusCode}");
+
+        Assert.IsTrue(response.IsSuccessStatusCode);
+    }
+
+    [TestMethod]
+    public async Task MakeGetRequestTo_ApiRequest_ShouldReturnOkay(){
+        var serviceHost = Environment.GetEnvironmentVariable("CICD_APP_SERVICE_HOSTNAME") ?? "localhost";
+
+        var httpClient = new HttpClient(){
+            BaseAddress = new Uri($"http://{serviceHost}:8197/")
+        };
+
+        var response = await httpClient.GetAsync("/request");
 
         Assert.IsTrue(response.IsSuccessStatusCode);
     }
