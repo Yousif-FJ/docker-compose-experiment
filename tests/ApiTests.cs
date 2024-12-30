0e20a87fe4be3f3bf4bf6dc50fa5c8cdc4b7389b
Yousif FJ <47510107+Yousif-FJ@users.noreply.github.com>
2024-12-30 19:42:28 +0300

    Pass service host name as an env variable

--- tests/ApiTests.cs
+++ tests/ApiTests.cs
@@ -10,8 +10,10 @@ public class ApiTests
     [TestMethod]
     public async Task MakeRequestToRestApi_ShouldReturnOkay()
     {
+        var serviceHost = Environment.GetEnvironmentVariable("CICD_APP_SERVICE_HOSTNAME") ?? "localhost";
+
         var httpClient = new HttpClient(){
-            BaseAddress = new Uri("http://docker:8198/")
+            BaseAddress = new Uri($"http://{serviceHost}:8198/")
         };
         var base64EncodedAuthString = Convert.ToBase64String(Encoding.UTF8.GetBytes("admin:admin"));
         httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + base64EncodedAuthString);
